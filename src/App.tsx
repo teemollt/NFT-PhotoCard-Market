a26693c038bd9c666f311419ce4879eccdacbf9f
ji55 <jsimbajs@gmail.com>
2021-09-06 20:27:17 +0900

    회원가입 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,186 +1,24 @@
 import React from 'react';
-import { GlobalStyle } from './App.style';
-import './App.css'
-import Button from '@material-ui/core/Button';
-import { TextField } from '@material-ui/core';
-import Input from '@material-ui/core/Input';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import IconButton from '@material-ui/core/IconButton';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import { useState } from 'react';
-
-interface State {
-  password: string;
-  showPassword: boolean;
-}
-
-
+import './App.css';
+import { Button } from '@material-ui/core';
+import Join from './join'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  const [password, setPassword] = useState('')
-  const [showPassword, setShowPassword] = useState(false)
-
-  const handleChange = (props: keyof State) => (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value)
-  }
-
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword)
+  const handleToJoin = (e: any) => {
+    window.location.replace("/join")
   }
 
   return (
-    <div>
-      <GlobalStyle />
-      <h1 className="signupTitle">JOIN</h1>
-      <div className="signupBox">
-        <div className="signupLeft">
-          <span>기본정보</span>
-          <span className="signupRequired"><span>●</span> 필수 입력 사항</span>
-          <hr style={{ height: "1px", backgroundColor:"black"}}/>
-          <div className="signupInfo">
-            <table className="signupTable">
-              <tbody>
-                <tr id="signupMobile">
-                  <th>
-                    <span className="require">●</span>
-                    <span>본인인증</span>
-                  </th>
-                  <td>
-                    <p>
-                      <span>휴대폰 인증</span>
-                      <Button className="signupInfoBtn" variant="outlined" size="small">인증하기</Button>
-                      <p className="phoneInfo">본인 명의의 휴대폰으로 본인인증을 진행합니다</p>
-                    </p>
-                  </td>
-                </tr>
-
-                <tr id="signupName">
-                  <th>
-                    <span className="require">●</span>
-                    <span>이름</span>
-                  </th>
-                  <td>
-                    <TextField id="standard-basic" />
-                  </td>
-                </tr>
-
-                <tr id="signupName">
-                  <th>
-                    <span className="require">●</span>
-                    <span>생년월일</span>
-                  </th>
-                  <td>
-                    <TextField style={{width: 80}}/><span style={{marginRight: 7}}>년</span>
-                    <TextField style={{width: 50}}/><span style={{marginRight: 7}}>월</span>
-                    <TextField style={{width: 50}}/><span>일</span>
-                  </td>
-                </tr>
-
-                <tr id="signupMobile">
-                  <th>
-                    <span className="require">●</span>
-                    <span>휴대폰</span>
-                  </th>
-                  <td>
-                    <TextField style={{width: 50}} defaultValue="010"/><span style={{marginRight: 7, marginLeft: 7}}>-</span>
-                    <TextField style={{width: 60}}/><span style={{marginRight: 7, marginLeft: 7}}>-</span>
-                    <TextField style={{width: 60}}/>
-                  </td>
-                </tr>
-
-                <tr id="signupId">
-                  <th>
-                    <span className="require">●</span>
-                    <span>아이디</span>
-                  </th>
-                  <td>
-                    <TextField id="standard-basic"/>
-                    <p className="phoneInfo" style={{marginTop: 5}}>영문소문자/숫자를 포함 6~16자 이내</p>
-                  </td>
-                </tr>
-
-                <tr id="signupPw">
-                  <th>
-                    <span className="require">●</span>
-                    <span>비밀번호</span>
-                  </th>
-                  <td>
-                    <Input
-                      id="standard-adornment-password"
-                      type={showPassword ? 'text' : 'password'}
-                      value={password}
-                      onChange={handleChange('password')}
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                          >
-                            {showPassword ? <Visibility /> : <VisibilityOff />}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                    />
-                    <p className="phoneInfo" style={{marginTop: 5}}>영문/숫자/ 특수문자 조합 10~16자</p>
-                  </td>
-                </tr>
-
-                <tr id="signupPwCheck">
-                  <th>
-                    <span className="require">●</span>
-                    <span>비밀번호 확인</span>
-                  </th>
-                  <td>
-                    <Input
-                      id="standard-adornment-password"
-                      type={showPassword ? 'text' : 'password'}
-                      value={password}
-                      onChange={handleChange('password')}
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                          >
-                            {showPassword ? <Visibility /> : <VisibilityOff />}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                    />
-                  </td>
-                </tr>
-
-                <tr id="signupNickname">
-                  <th>
-                    <span className="require">●</span>
-                    <span>닉네임</span>
-                  </th>
-                  <td>
-                    <TextField id="standard-basic"/>
-                    <Button className="signupInfoBtn" variant="outlined" size="small">중복 확인</Button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-
-            <div className="signupSelectCeleb">
-              <span className="require">●</span>
-              <span>CELEB 선택</span>
-              <span className="phoneInfo" style={{marginLeft: 15}}>좋아하는 CELEB을 선택해 주세요</span>
-              <div>
-                <p className="signupCelebBox">ds</p>
-                <p className="signupCelebBox"></p>
-              </div>
-              
-            </div>
-          </div>
-        </div>
-
-        <div className="signupRight">
-          <span>약관정보</span>
-        </div>
-      </div>
+    <div className="App">
+      <Button variant="contained" color="primary" onClick={handleToJoin}>
+        Join
+      </Button>
+      <Switch>
+        <Route path="/join">
+          <Join />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/join.tsx
@@ -0,0 +1,253 @@
+import React, { useState } from "react";
+import { TextField, Button } from '@material-ui/core';
+import Snackbar, { SnackbarOrigin } from '@material-ui/core/Snackbar';
+import "./join.css"
+import { AnyRecord } from "dns";
+
+export interface State {
+  userId: string,
+  password1: string,
+  password2: string,
+  nickname: string,
+  email: string,
+  checkEmail: boolean,
+  snackId: boolean,
+  snackNick: boolean,
+  celeb: string
+}
+
+function Join () {
+  const [userId, setUserId] = useState('')
+  const [password1, setPassword1] = useState('')
+  const [password2, setPassword2] = useState('')
+  const [nickname, setNickname] = useState('')
+  const [email, setEmail] = useState('')
+  const [checkEmail, setCheckEmail] = useState(true)
+  const [snackId, setSnackId] = useState(false)
+  const [snackNick, setSnackNick] = useState(false)
+  const [celeb, setCeleb] = useState('')
+
+  const handleUserId = (e: any) => {
+    setUserId(e.target.value)
+  }
+
+  const handleUserIdDupl = (newSnack: SnackbarOrigin) => (e: any) => {
+    setSnackId(true)
+  }
+
+  const handleSnackIdClose = (e: any) => {
+    setSnackId(false)
+  }
+
+  const handlePassword1 = (e: any) => {
+    setPassword1(e.target.value)
+  }
+
+  const handlePassword2 = (e: any) => {
+    setPassword2(e.target.value)
+  }
+
+  const handleNick = (e: any) => {
+    setNickname(e.target.value)
+  }
+
+  const handleNickDupl = (newSnack: SnackbarOrigin) => (e: any) => {
+    setSnackNick(true)
+  }
+
+  const handleSnackNickClose = (e: any) => {
+    setSnackNick(false)
+  }
+
+  const handleEmail = (e: any) => {
+    setEmail(e.target.value)
+    if (e.target.value.match(/^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i)){
+      setCheckEmail(true)
+    } else {
+      setCheckEmail(false)
+    }
+  }
+
+  const handleCeleb = (e: any) => {
+    const choice = e.target.innerText
+    if (choice === "김도형") {
+      setCeleb("김도형")
+    } else if (choice === "나비") {
+      setCeleb("나비")
+    }else if (choice === "남근형") {
+      setCeleb("남근형")
+    }else if (choice === "신지현") {
+      setCeleb("신지현")
+    }else if (choice === "조영우") {
+      setCeleb("조영우")
+    }else if (choice === "하지훈") {
+      setCeleb("하지훈")
+    }
+  }
+
+
+  const handleBackToMain = (e: any) => {
+    window.location.replace("/")
+  }
+
+  return (
+    <div className="join">
+      <table className="joinTable">
+        <h1>JOIN</h1>
+        <tbody>
+          <tr id="joinId">
+            <th>
+              <span className="require">●</span>
+              <span>아이디</span>
+            </th>
+            <td>
+              <TextField id="standard-basic" onChange={handleUserId}/>
+              <Button 
+                className="joinDuplBtn" 
+                variant="outlined" 
+                size="small" 
+                onClick={handleUserIdDupl({vertical: 'top', horizontal: 'right'})}
+              >
+                중복 확인
+              </Button>
+              <Snackbar
+                className="joinSnack"
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                open={snackId}
+                onClose={handleSnackIdClose}
+                message="사용 가능한 아이디입니다"
+              />
+            </td>
+          </tr>
+
+          <tr id="joinPw1">
+            <th>
+              <span className="require">●</span>
+              <span>비밀번호</span>
+            </th>
+            <td>
+              <TextField id="standard-basic" type="password" onChange={handlePassword1}/>
+            </td>
+          </tr>
+
+          <tr id="joinPw2">
+            <th>
+              <span className="require">●</span>
+              <span>비밀번호 확인</span>
+            </th>
+            <td>
+              <TextField 
+                id="standard-basic" 
+                type="password" 
+                onChange={handlePassword2} 
+                helperText={ password1 !== password2 ? "비밀번호가 일치하지 않습니다" : ''}
+                error={ password1 !== password2 ? true : false}/>
+            </td>
+          </tr>
+
+          <tr id="joinNick">
+            <th>
+              <span className="require">●</span>
+              <span>닉네임</span>
+            </th>
+            <td>
+              <TextField id="standard-basic" onChange={handleNick}/>
+              <Button 
+                className="joinDuplBtn" 
+                variant="outlined" 
+                size="small" 
+                onClick={handleNickDupl({vertical: 'top', horizontal: 'right'})}
+              >
+              중복 확인</Button>
+              <Snackbar
+                className="joinSnack"
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                open={snackNick}
+                onClose={handleSnackNickClose}
+                message="사용 가능한 닉네임입니다"
+              />
+            </td>
+          </tr>
+
+          <tr id="joinNick">
+            <th>
+              <span className="require">●</span>
+              <span>이메일</span>
+            </th>
+            <td>
+              <TextField 
+                id="standard-basic" 
+                type="email" 
+                onChange={handleEmail}
+                autoComplete="email"
+                helperText={ checkEmail ? '' :"유효하지 않은 메일입니다"}
+                error={ checkEmail ? false : true }
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div className="joinSelectCeleb">
+        <div className="joinCelebInfo">
+          <span className="require">●</span>
+          <span>CELEB 선택</span>
+          <span className="joinCelebSubInfo">좋아하는 CELEB을 선택해 주세요</span>
+        </div>
+        <div className="joinCelebBoxs">
+          <div 
+            className="joinCelebBox joinKim" 
+            onClick={handleCeleb} 
+            style={{ backgroundColor: celeb === "김도형" ? "rgba(16, 0, 247, 0.329)" : undefined }}
+          >
+            김도형
+          </div>
+          <div 
+            className="joinCelebBox joinNa" 
+            onClick={handleCeleb} 
+            style={{ backgroundColor: celeb === "나비" ? "rgb(232, 248, 10, 0.329)" : undefined }}
+          >
+            나비
+          </div>
+          <div 
+            className="joinCelebBox joinNam" 
+            onClick={handleCeleb}
+            style={{ backgroundColor: celeb === "남근형" ? "rgb(241, 162, 13, 0.329)" : undefined }}
+          >
+            남근형
+          </div>
+          <div 
+            className="joinCelebBox joinShin"
+            onClick={handleCeleb}
+            style={{ backgroundColor: celeb === "신지현" ? "rgba(127, 12, 235, 0.329)" : undefined }}
+          >
+            신지현
+          </div>
+          <div 
+            className="joinCelebBox joinCho"
+            onClick={handleCeleb}
+            style={{ backgroundColor: celeb === "조영우" ? "rgba(24, 248, 4, 0.329)" : undefined }}
+          >
+            조영우
+          </div>
+          <div 
+            className="joinCelebBox joinHa"
+            onClick={handleCeleb}
+            style={{ backgroundColor: celeb === "하지훈" ? "rgb(7, 182, 252, 0.329)" : undefined }}
+          >
+            하지훈
+          </div>
+        </div>
+      </div>
+
+      <br /><br /><br />
+
+      <div className="joinBtns">
+        <Button className="joinCancelBtn" variant="contained" onClick={handleBackToMain}>취소</Button>
+        <Button className="joinBtn" variant="contained" color="primary">회원가입</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Join;
