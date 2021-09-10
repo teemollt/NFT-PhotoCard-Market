8945948f0fb610ae5ffd6d00537e921c4e6452ef
ji55 <jsimbajs@gmail.com>
2021-09-10 14:47:22 +0900

    [FRONTEND] 정보 수정 취소 버튼 수정

--- frontend/src/components/account/mypage/update/UpdateBtns.tsx
+++ frontend/src/components/account/mypage/update/UpdateBtns.tsx
@@ -7,7 +7,7 @@ function UpdateBtns() {
 
   return (
     <div className="updateBtns">
-      <Link className="updateCanclelink" to="/mypage">
+      <Link className="updateCanclelink" to="/mypage" style={{ textDecoration: "none" }}>
         <Button className="updateCancelBtn" variant="contained">취소</Button>
       </Link>
       <Button className="updateBtn" variant="contained" color="primary">수정</Button>
