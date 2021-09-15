242b0fa4207a58a53e6ed1b6950f5a68ff13c12b
남근형 <magicom98@gmail.com>
2021-09-15 22:55:44 +0900

    fix urlerror

--- backend/src/main/java/com/blockChain/controller/CardPackController.java
+++ backend/src/main/java/com/blockChain/controller/CardPackController.java
@@ -4,20 +4,23 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blockChain.service.SalesSvcInter;
 
 @RestController
-@RequestMapping("/api/saleCard")
-public class SaleCardController {
-	
+@RequestMapping("/api/cardPack")
+public class CardPackController {
 	@Autowired
 	SalesSvcInter salesSvc;
-	@GetMapping
-	public Map<String,Object> sltSalesByMW(){
-		return salesSvc.sltSalesByMW();
+	@GetMapping("/{cardpackPK}/review")
+	public Map<String,Object> sltReviewList(
+		    @PathVariable(name="cardpackPK") Long cardpackPK
+			){
+		System.out.println("here1");
+		return salesSvc.sltReviewList(cardpackPK);
+		
 	}
-	
 }
