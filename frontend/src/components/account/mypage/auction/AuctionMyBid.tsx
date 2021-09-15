7056405270723e3d8b6abb7ffb945379d25b658a
ji55 <jsimbajs@gmail.com>
2021-09-15 14:28:54 +0900

    [FRONTEND] 경매 내역 수정

--- frontend/src/components/account/mypage/auction/AuctionMyBid.tsx
+++ frontend/src/components/account/mypage/auction/AuctionMyBid.tsx
@@ -3,6 +3,17 @@ import AuctionCard from "./AuctionCard";
 
 const tempCard: Array<tempCard> = [
   {
+    imgUrl: "https://newsimg.sedaily.com/2021/03/24/22JXKJ0JJZ_1.jpg",
+    title: "블랙야크",
+    price: 1,
+  },
+  {
+    imgUrl:
+    "https://img4.yna.co.kr/photo/cms/2019/05/02/02/PCM20190502000402370_P2.jpg",
+    title: "연합뉴스",
+    price: 1,
+  },
+  {
     imgUrl:
       "https://cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/XG2MW2H3ZRW5FHDVSOMF6FDT3E.jpg",
     title: "아이유꽃",
@@ -10,8 +21,8 @@ const tempCard: Array<tempCard> = [
   },
   {
     imgUrl:
-      "https://img4.yna.co.kr/photo/cms/2019/05/02/02/PCM20190502000402370_P2.jpg",
-    title: "연합뉴스",
+      "http://www.polinews.co.kr/data/photos/20200834/art_15980031118376_e6a761.jpg",
+    title: "정장",
     price: 1,
   },
   {
@@ -24,17 +35,6 @@ const tempCard: Array<tempCard> = [
     imgUrl:
       "https://cdn.dailyimpact.co.kr/news/photo/201901/50650_10024_2221.jpg",
     title: "흑백",
-    price: 1,
-  },
-  {
-    imgUrl:
-      "http://www.polinews.co.kr/data/photos/20200834/art_15980031118376_e6a761.jpg",
-    title: "정장",
-    price: 1,
-  },
-  {
-    imgUrl: "https://newsimg.sedaily.com/2021/03/24/22JXKJ0JJZ_1.jpg",
-    title: "블랙야크",
     price: 1,
   },
 ];
