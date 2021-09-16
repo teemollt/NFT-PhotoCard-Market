3690434e952dc333c4640a253ebadc1a5c241f5e
남근형 <magicom98@gmail.com>
2021-09-16 16:40:47 +0900

    fix timeZone

--- backend/src/main/java/com/blockChain/BackendApplication.java
+++ backend/src/main/java/com/blockChain/BackendApplication.java
@@ -1,5 +1,9 @@
 package com.blockChain;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -11,5 +15,10 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+	@PostConstruct
+	public void initApplication() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 
 }
