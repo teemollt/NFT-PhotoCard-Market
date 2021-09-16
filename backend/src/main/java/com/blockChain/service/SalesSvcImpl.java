88c198c2b67687ee0a8e351b577b23655ab0ad14
남근형 <magicom98@gmail.com>
2021-09-16 15:37:38 +0900

    fix updateMember add insertReview

--- backend/src/main/java/com/blockChain/service/SalesSvcImpl.java
+++ backend/src/main/java/com/blockChain/service/SalesSvcImpl.java
@@ -1,5 +1,6 @@
 package com.blockChain.service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -12,7 +13,12 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.blockChain.config.SecurityUtil;
+import com.blockChain.domain.Member;
+import com.blockChain.domain.Reply;
+import com.blockChain.domain.Sales;
 import com.blockChain.dto.SalesDTO;
+import com.blockChain.repository.MemberRepo;
 import com.blockChain.repository.ReplyRepo;
 import com.blockChain.repository.SalesRepo;
 
@@ -27,6 +33,8 @@ public class SalesSvcImpl implements SalesSvcInter{
 	SalesRepo salesRepo;
 	@Autowired
 	ReplyRepo replyRepo;
+	@Autowired
+	private MemberRepo memberRepo;
 	@Override
 	public Map<String,Object>sltSalesByMW(){
 		Map<String, Object> res = new HashMap<String,Object>();
@@ -47,6 +55,31 @@ public class SalesSvcImpl implements SalesSvcInter{
 		Map<String, Object> res = new HashMap<String,Object>();
 		System.out.println("here2");
 		res.put("res", replyRepo.sltReviewList(cardpackPK));
+		return res;
+	}
+	
+	@Override
+	public Map<String,Object>insertReview(long cardpackPK,Map<String, Object> req){
+		Map<String, Object> res = new HashMap<String,Object>();
+//		Optional<Sales> sales = salesRepo.findById(cardpackPK);
+		
+		try {
+			Member member = memberRepo.findById(SecurityUtil.getCurrentMemberId()).orElseThrow(() -> new IllegalStateException("로그인 유저정보가 없습니다"));
+			Optional<Sales> sales = Optional.ofNullable(salesRepo.findById(cardpackPK).orElseThrow(()->new IllegalStateException("존재하지 않는 카드번호입니다.")));
+			Reply reply = new Reply();
+			reply.setMember(member);
+			reply.setReplyContent((String)req.get("reviewContent"));
+			reply.setSales(sales.get());
+			reply.setReplyDate(LocalDateTime.now());
+			Reply saved = replyRepo.save(reply);
+			res.put("success", true);
+			res.put("msg", "정상적으로 입력되었습니다.");
+		}catch(IllegalStateException e){
+			res.put("success", false);
+			res.put("msg", e.getMessage());
+			return res;
+			
+		}
 		return res;
 	}
 }
