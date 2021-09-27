af51db34a2323992273e4eef16660f39b8c2a2b6
하지훈 <gkwlsrnjs12@gmail.com>
2021-09-27 21:22:43 +0900

    wallet 생성

--- backend/src/main/java/com/blockChain/service/WalletSvcImpl.java
+++ backend/src/main/java/com/blockChain/service/WalletSvcImpl.java
@@ -9,8 +9,10 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.blockChain.config.SecurityUtil;
 import com.blockChain.domain.Member;
 import com.blockChain.domain.Wallets;
+import com.blockChain.repository.MemberRepo;
 import com.blockChain.repository.WalletRepo;
 
 @Service
@@ -19,26 +21,30 @@ public class WalletSvcImpl implements WalletSvcInter {
 
 	@Autowired
 	private WalletRepo walletRepo;
+	@Autowired
+	private MemberRepo memberRepo;
 	
 	@Override
 	public Map<String, Object> register(Map<String, Object> req) {
 		Map<String, Object> res = new HashMap<String,Object>();
-		Member walletMember = (Member)req.get("walletMemeber");
-		String walletAdd = (String)req.get("walletAdd");
-		
+			
 		try {
+			Member member = memberRepo.findById(SecurityUtil.getCurrentMemberId()).orElseThrow(() -> new IllegalStateException("로그인 유저정보가 없습니다"));
+			String walletAdd = (String)req.get("walletAdd");
+			
 			Wallets wallet = new Wallets();
 			wallet.setWalletAdd(walletAdd);
 			wallet.setWalletBal(new BigDecimal(0));
+			wallet.setMember(member);
 			wallet.setWalletCash(0L);
 			wallet.setWalletRC(0L);
 			walletRepo.save(wallet);
 			
 			res.put("msg", "지갑등록 성공");
 		}catch(Exception e) {
-			
+			res.put("msg", e.getMessage());
 		}
-		return null;
+		return res;
 	}
 
 }
