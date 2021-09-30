312d70aed36b24d4878243af6ecc7547fe09f6fa
ji55 <jsimbajs@gmail.com>
2021-09-30 22:06:08 +0900

    [FRONTEND] 메인 영상

--- frontend/src/components/main/MainCarousel.tsx
+++ frontend/src/components/main/MainCarousel.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import CardMedia from "@material-ui/core/CardMedia";
 import Card from "@material-ui/core/Card";
+import axios from 'axios'
+
+
+
 function MainCarousel(): JSX.Element {
+  const [celeb, setCeleb] = useState<number>(5)
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      axios
+      .get("/api/member/mypage", {
+        headers: { Authorization: localStorage.getItem("token") },
+      })
+      .then((res)=>{
+        setCeleb(res.data.mypage.celebNo)
+      })
+    }
+    
+  })
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -11,7 +30,7 @@ function MainCarousel(): JSX.Element {
         <Card>
           <CardMedia
             component="video"
-            image="/videos/아이유화보영상.mp4"
+            image={"/videos/"+celeb+".mp4"}
             muted
             controls
             autoPlay
